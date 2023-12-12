37d1886be140f9889cb8eaba6963691f7bf00e22
Joe <95573961+The-Blue-W1zard@users.noreply.github.com>
2023-12-12 21:18:19 +0000

    OMG IT WORKS!!!!!!!

--- DIjkstras Search/Program.cs
+++ DIjkstras Search/Program.cs
@@ -26,76 +26,6 @@
             UpdateMaze(ref maze, path);
             OutputMaze(maze);
         }
-        static List<Point> DijkstrasAlgorithm(int[,] maze, Point start, Point end)
-
-        {
-            PriorityQueue<Point,int> Q = new PriorityQueue<Point,int>();
-            Dictionary<Point, int> dist = new Dictionary<Point, int>();
-            Dictionary<Point, Point> cameFrom = new Dictionary<Point, Point>();
-            Point None = new Point(-1, -1);
-
-            for (int row = 0; row < maze.GetLength(0); row++) 
-            {
-                for (int col = 0; col < maze.GetLength(1); col++)
-                {
-                    Point point = new Point(row,col);
-                    dist.Add(point, int.MaxValue);
-                    cameFrom.Add(point, None);
-                    Q.Enqueue(point, dist[point]);
-                }
-            }
-
-            dist[start] = 0;
-            Q.Enqueue(start, 0);
-            Point currentCell = None;
-
-            while(Q.Count > 0)
-            {
-                currentCell = Q.Dequeue();
-
-                if (currentCell == end)
-                {
-                    Console.WriteLine("Reached end");
-                    break;
-                }
-                Console.WriteLine("Current Cell + " +currentCell);
-
-
-                foreach(Point nextCell in Neighbours(maze, currentCell))
-                {
-                    if(!Contains(Q, nextCell)) { continue; }
-
-                    int newDistance = dist[currentCell] + 1;
-                    Console.WriteLine(nextCell + "," + newDistance + ","+dist[nextCell]);
-                    if (newDistance < dist[nextCell])
-                    {
-                        Console.WriteLine("updating");
-                        Q.Enqueue(nextCell, newDistance);
-                        dist[nextCell] = newDistance;
-                        cameFrom[nextCell] = currentCell;
-                       
-                    }
-                }
-                Console.Write("outputting Q:");
-                OutputPQ(Q);
-                
-            } 
-
-            if (cameFrom[end] == None)
-            {
-                Console.WriteLine("No path found");
-            }
-
-            List<Point> path = new List<Point>();
-            currentCell = end;
-            while (currentCell != None)
-            {
-                path.Add(currentCell);
-                currentCell = cameFrom[currentCell];
-            }
-            path.Reverse();
-            return path;
-        }
 
         static List<Point> DijkstraVersion2(int[,] maze, Point start, Point goal)
         {
@@ -152,32 +82,7 @@
             path.Reverse();
             return path;
         }
-        static bool Contains(PriorityQueue<Point, int> q, Point a )
-        {
-            for (int i = 0; i < q.Count; i++)
-            {
-                Point test = q.Dequeue();
-                if (test.X == a.X && test.Y == a.Y)
-                {
-                    return true;
-                }
-            }
-            return false;
-
-        }
-        static void OutputPQ (PriorityQueue<Point,int> q)
-        {
-            for(int i = 0; i < q.Count; i++)
-            {
-                Point test = q.Dequeue();
-                Console.Write(test);
-            }
-            Console.WriteLine(" " + q.Count);
-
-        }
-      
-
-        
+            
         static List<Point> Neighbours(int[,] maze, Point current)
         {
             int[] checksRows = { 0, 0, 1, -1 };
@@ -236,14 +141,16 @@
 
         static void UpdateMaze(ref int[,] maze, List<Point> path)
         {
-            try
+            for (int i = 0; i < path.Count(); i++)
             {
-                for (int i = 0; i < path.Count(); i++)
+                try
                 {
-                    maze[path[i].Y, path[i].X] = 9;
+                    if(maze[path[i].X, path[i].Y] == 1) { Console.WriteLine("mucked up here") ; continue; }
+                    maze[path[i].X, path[i].Y] = 9;
                 }
+                catch { Console.WriteLine("coordinates bad"); }
             }
-            catch { }
+            
         }
 
     }
